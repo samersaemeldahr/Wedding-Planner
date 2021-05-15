e1c3023d82e86711cf4f00e63876306a2e38d841
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-15 20:41:52 +0300

    Create registry model

---
+++ models/Registry.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Registry extends Model { }
+
+Registry.init(
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        link: {
+            type: DataTypes.STRING,
+            validate: {
+                isUrl: true,
+            }
+        }
+    },
+    {
+        sequelize
+    }
+)
+
+module.exports = Registry
