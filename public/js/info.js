c39e2a7477034246c835c894fc073ba3411a320e
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-22 21:40:51 +0300

    Add info function

---
+++ public/js/info.js
@@ -0,0 +1,28 @@
+const infoData = async function(event) {
+    event.preventDefault();
+  
+    const greeting = document.querySelector('#greeting').value;
+    const catering = document.querySelector('#catering').value;
+    const photographer = document.querySelector('#photographer').value;
+    const dj = document.querySelector('#dj').value;
+    const kids = document.querySelector('#kids').value;
+
+    await fetch(`/api/info`, {
+      method: 'POST',
+      body: JSON.stringify({
+        greeting,
+        catering,
+        photographer,
+        dj,
+        kids
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    document.location.replace('/dashboard');
+  };
+  
+  document
+    .querySelector('#info-form')
+    .addEventListener('submit', infoData);
+  
