8fb9463e49a2c9d6dea2b018b4c6b6c615d4a63a
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-05-22 18:01:55 +0300

    Edit routes

--- controllers/api/questions-routes.js
+++ controllers/api/questions-routes.js
@@ -1,2 +1,19 @@
 const router = require("express").Router();
 const Questions = require("../../models/Questions.js"); //referencing the Questions model
+
+router.post('/', /* withAuth, */ async (req, res) => {
+    try {
+        const questions = await Questions.create({
+            firstName: req.body.firstName,
+            spouseName: req.body.spouseName,
+            weddingDate: req.body.weddingDate,
+            venueName: req.body.venueName,
+            venueLocation: req.body.venueLocation
+        });
+        res.json(questions);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router
